675ca3fde38ca9e6411401ef21bba08492c22457
Hans <94590267+HansHans135@users.noreply.github.com>
2022-07-05 21:45:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,19 +20,24 @@ async def on_message(message):
                 with open(f"data/{message.channel.id}.txt", 'a') as filt:
                     filt.write(f'開始時間:{now}\n頻道名稱:{message.channel.name}\n開始人:{message.author}\n以下為詳細的對話紀錄:\n\n\n')
                 await message.channel.send('已開始錄製!!')
-            await message.channel.send('已經在錄製!!')
+            else:
+                await message.channel.send('已經在錄製!!')
                 
     if message.content == 'h!recstop':
         if message.author.guild_permissions.manage_messages:
-            with open(f"data/{message.channel.id}.txt", 'a') as filt:
-                filt.write(f'\n\n結束人:{message.author}\n結束時間:{now}')
-            await message.channel.send('已結束錄製!!', file=discord.File(f"data/{message.channel.id}.txt"))
-            fileTest = f"data/{message.channel.id}.txt"
-            os.remove(fileTest)
+            filepath = f"data/{message.channel.id}.txt"
+            if os.path.isfile(filepath):
+                with open(f"data/{message.channel.id}.txt", 'a') as filt:
+                    filt.write(f'\n\n結束人:{message.author}\n結束時間:{now}')
+                await message.channel.send('已結束錄製!!', file=discord.File(f"data/{message.channel.id}.txt"))
+                fileTest = f"data/{message.channel.id}.txt"
+                os.remove(fileTest)
+            else:
+                await message.channel.send('已結束錄製!!', file=discord.File(f"data/{message.channel.id}.txt"))
             
     filepath = f"data/{message.channel.id}.txt"
     if os.path.isfile(filepath):
         with open(f"data/{message.channel.id}.txt", 'a') as filt:
             filt.write(f'{now}|{message.author}:{message.content}\n')
         
-client.run("owo") 
+client.run("TOKEN") 
